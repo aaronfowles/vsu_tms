a780503a13673d0c5acf752db2be146e2b31a404
Aaron Fowles <aaronfowles@gmail.com>
2016-03-18 14:27:32 -0400

    logic for my_tasks started

--- vsu_tms_site/vsu_tms_app/views.py
+++ vsu_tms_site/vsu_tms_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db import connection
 
-from .models import TaskListItem, Task
+from .models import TaskListItem, Task, Staff, Role, LookupTaskUrgency
 
 
 # Landing page
@@ -22,9 +23,27 @@ def my_tasks(req):
     context = {}
     user = req.user.id
     staff = Staff.objects.get(user_id=user).id
-    role = Staff.objects.get(staff_id=staff).id
-    all_incomplete_and_for_role = TaskListItem.filter(complete=False).select_related().filter(role_id=role)
-    context['my_tasks_items'] = all_incomplete_and_for_role
+    role = Staff.objects.get(id=staff).role_id
+    
+    context['my_tasks_items'] = []
+
+    incomplete_tasks = TaskListItem.objects.filter(complete=False)
+    tasks_assigned_to_role = Task.objects.filter(assigned_role_id=role)
+
+    task_ids_assigned_to_role = [task.id for task in tasks_assigned_to_role]
+
+    for task in incomplete_tasks:
+        temp_dict = {}
+        if task.task_id.id in task_ids_assigned_to_role:
+            temp_dict['tasklist_id'] = task.tasklist_id
+            temp_dict['task_id'] = task.task_id
+            urgency = Task.objects.get(id=task.task_id.id).task_urgency_id
+            urgency = LookupTaskUrgency.objects.get(id=urgency.id).urgency
+            temp_dict['urgency'] = urgency
+            temp_dict['task_desc'] = Task.objects.get(id=task.task_id.id).task_desc
+            temp_dict['time_due'] = task.time_due
+            context['my_tasks_items'].append(dict(temp_dict))
+
     return render(req,'my_tasks.html', context)
 
 # Daily Management Page
