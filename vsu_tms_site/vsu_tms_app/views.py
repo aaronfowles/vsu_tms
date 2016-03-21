3f798c2d92b4889b5acdbe888a09c69e0f3e1d5e
Aaron Fowles <aaronfowles@gmail.com>
2016-03-21 06:15:48 -0400

    Display tables underway

--- vsu_tms_site/vsu_tms_app/views.py
+++ vsu_tms_site/vsu_tms_app/views.py
@@ -14,7 +14,18 @@ def index(req):
 def home(req):
     context = {}
     all_incomplete = TaskListItem.objects.filter(complete=False).select_related()
-    context['all_tasklist_items'] = all_incomplete
+    context['all_tasklist_items'] = []
+    for tasklist_item in all_incomplete:
+        temp_dict = {}
+        temp_dict['tasklist_id'] = tasklist_item.id
+        temp_dict['time_due'] = tasklist_item.time_due
+        temp_dict['task_id'] = tasklist_item.task_id.id
+        task_obj = Task.objects.get(id=tasklist_item.task_id.id)
+        temp_dict['task_desc'] = task_obj.task_desc
+        temp_dict['assigned_role'] = task_obj.assigned_role_id
+        urgency = LookupTaskUrgency.objects.get(id=task_obj.task_urgency_id.id)
+        temp_dict['urgency'] = urgency
+        context['all_tasklist_items'].append(dict(temp_dict))
     return render(req, 'home.html', context)
 
 # My Tasks page
@@ -35,13 +46,15 @@ def my_tasks(req):
     for task in incomplete_tasks:
         temp_dict = {}
         if task.task_id.id in task_ids_assigned_to_role:
-            temp_dict['tasklist_id'] = task.tasklist_id
-            temp_dict['task_id'] = task.task_id
-            urgency = Task.objects.get(id=task.task_id.id).task_urgency_id
-            urgency = LookupTaskUrgency.objects.get(id=urgency.id).urgency
+            temp_dict['tasklist_id'] = task.tasklist_id.id
+            temp_dict['task_id'] = task.task_id.id
+            urgency_id = Task.objects.get(id=task.task_id.id).task_urgency_id.id
+            urgency = LookupTaskUrgency.objects.get(id=urgency_id).urgency
             temp_dict['urgency'] = urgency
-            temp_dict['task_desc'] = Task.objects.get(id=task.task_id.id).task_desc
+            temp_dict['task_desc'] = task.task_desc
             temp_dict['time_due'] = task.time_due
+            role_id = Task.objects.get(id=task.task_id.id).assigned_role_id.id
+            temp_dict['assigned_role'] = role_id           
             context['my_tasks_items'].append(dict(temp_dict))
 
     return render(req,'my_tasks.html', context)
