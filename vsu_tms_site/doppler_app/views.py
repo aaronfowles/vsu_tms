415cba9be96b83a5a2151a389f042a3df8fcfda9
Aaron Fowles <aaronfowles@gmail.com>
2016-05-12 06:30:55 -0400

    num_peaks finder and results page added

--- vsu_tms_site/doppler_app/views.py
+++ vsu_tms_site/doppler_app/views.py
@@ -26,4 +26,48 @@ def upload_doppler(request):
 
     rate, wf = scipy.io.wavfile.read(dest_file_handle)
 
-    return JsonResponse({'received':'true','saved':'false'}) 
+    def find_waveform_start(wf):
+        # Band-pass filtering
+        nyq = 0.5 * 44100
+        cutoff = 250 / nyq
+        b, a = signal.butter(4,cutoff,'highpass')
+        wf = signal.lfilter(b, a, wf)
+        cutoff = 1000 / nyq
+        b, a = signal.butter(4,cutoff,'lowpass')
+        wf = signal.lfilter(b, a, wf)
+        # Normalise waveform
+        peak_amplitude = wf.max()
+        wf = wf / float(peak_amplitude)
+        # calculate magnitude
+        wf_for_grad = [i if i > 0 else i*(-1) for i in wf]
+        # Calculate gradients and normalise them
+        wf_grad = np.gradient(wf_for_grad)
+        peak_gradient = wf_grad.max()
+        wf_grad = wf_grad / float(peak_gradient)
+        # Waveform finding logic
+        start_indices = []
+        amplitude_threshold = 0.3
+        gradient_threshold = 0.3
+        running_lag = 0
+        running_lag_threshold = 10000
+        for i in range(0,len(wf)):
+            if (wf[i] > amplitude_threshold and wf_grad[i] > gradient_threshold and running_lag >= running_lag_threshold):
+                start_indices.append(1)
+                running_lag = 0
+            else:
+                start_indices.append(0)
+                running_lag += 1
+        return wf, start_indices
+
+    wf, start_indices = find_waveform_start(wf[:,0])
+
+    context = {}
+    context['sample_length_secs'] = len(wf) / float(rate)
+
+    num_points = 0
+    for i in range(0,len(start_indices)):
+        if (start_indices[i] is not 0):
+            num_points[str(i)] += 1
+    context['num_waveforms'] = num_points
+
+    return render(request,'results.html', context)
