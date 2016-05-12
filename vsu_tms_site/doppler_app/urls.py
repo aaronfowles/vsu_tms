a87d3c8e530fa008f2196594d3b24656391a6a7f
Aaron Fowles <aaronfowles@gmail.com>
2016-05-12 08:47:41 -0400

    audio upload broken

--- vsu_tms_site/doppler_app/urls.py
+++ vsu_tms_site/doppler_app/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^upload_doppler/', views.upload_doppler, name='upload_doppler'),
-    url(r'^results/', views.results, name='results'),
 ]
--- vsu_tms_site/doppler_app/views.py
+++ vsu_tms_site/doppler_app/views.py
@@ -4,17 +4,18 @@ from django.http import JsonResponse
 from StringIO import StringIO
 import wave
 import scipy.io.wavfile
+from scipy import signal
 import numpy as np
 from datetime import datetime
 from os import getcwd
 from os.path import join, normpath
-
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def upload_doppler(request):
+    print('upload doppler called')
     doppler_blob = request.FILES['audio']
 
     dest_file_handle = 'doppler_audio_samples/' + str(datetime.now().strftime('%Y-%m-%d_%H-%M'))
@@ -26,40 +27,39 @@ def upload_doppler(request):
 
     rate, wf = scipy.io.wavfile.read(dest_file_handle)
 
-    def find_waveform_start(wf):
-        # Band-pass filtering
-        nyq = 0.5 * 44100
-        cutoff = 250 / nyq
-        b, a = signal.butter(4,cutoff,'highpass')
-        wf = signal.lfilter(b, a, wf)
-        cutoff = 1000 / nyq
-        b, a = signal.butter(4,cutoff,'lowpass')
-        wf = signal.lfilter(b, a, wf)
-        # Normalise waveform
-        peak_amplitude = wf.max()
-        wf = wf / float(peak_amplitude)
-        # calculate magnitude
-        wf_for_grad = [i if i > 0 else i*(-1) for i in wf]
-        # Calculate gradients and normalise them
-        wf_grad = np.gradient(wf_for_grad)
-        peak_gradient = wf_grad.max()
-        wf_grad = wf_grad / float(peak_gradient)
-        # Waveform finding logic
-        start_indices = []
-        amplitude_threshold = 0.3
-        gradient_threshold = 0.3
-        running_lag = 0
-        running_lag_threshold = 10000
-        for i in range(0,len(wf)):
-            if (wf[i] > amplitude_threshold and wf_grad[i] > gradient_threshold and running_lag >= running_lag_threshold):
-                start_indices.append(1)
-                running_lag = 0
-            else:
-                start_indices.append(0)
-                running_lag += 1
-        return wf, start_indices
+    wf = wf[:,0]
 
-    wf, start_indices = find_waveform_start(wf[:,0])
+    # Band-pass filtering
+    print('find waveform called')
+    nyq = 0.5 * 44100
+    cutoff = 250 / nyq
+    b, a = signal.butter(4,cutoff,'highpass')
+    wf = signal.lfilter(b, a, wf)
+    cutoff = 1000 / nyq
+    b, a = signal.butter(4,cutoff,'lowpass')
+    wf = signal.lfilter(b, a, wf)
+    # Normalise waveform
+    peak_amplitude = wf.max()
+    wf = wf / float(peak_amplitude)
+    # calculate magnitude
+    wf_for_grad = [i if i > 0 else i*(-1) for i in wf]
+    # Calculate gradients and normalise them
+    wf_grad = np.gradient(wf_for_grad)
+    peak_gradient = wf_grad.max()
+    wf_grad = wf_grad / float(peak_gradient)
+    # Waveform finding logic
+    start_indices = []
+    amplitude_threshold = 0.3
+    gradient_threshold = 0.3
+    running_lag = 0
+    running_lag_threshold = 10000
+    for i in range(0,len(wf)):
+        if (wf[i] > amplitude_threshold and wf_grad[i] > gradient_threshold and running_lag >= running_lag_threshold):
+            start_indices.append(1)
+            running_lag = 0
+        else:
+            start_indices.append(0)
+            running_lag += 1
 
     context = {}
     context['sample_length_secs'] = len(wf) / float(rate)
