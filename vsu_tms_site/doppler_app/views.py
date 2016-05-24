ce33fde1a8af83133c7a48ea8f8e669d829be9e9
Aaron Fowles <aaronfowles@gmail.com>
2016-05-24 06:19:14 -0400

    thresholds updated on live algorithm

--- vsu_tms_site/doppler_app/views.py
+++ vsu_tms_site/doppler_app/views.py
@@ -38,22 +38,23 @@ def upload_doppler(request):
     wf = signal.lfilter(b, a, wf)
     # Normalise waveform
     peak_amplitude = wf.max()
-    wf = wf / float(peak_amplitude)
+    wf_normed = wf / float(peak_amplitude)
     # calculate magnitude
-    wf_for_grad = [i if i > 0 else i*(-1) for i in wf]
+    wf_for_grad = [i if i > 0 else i*(-1) for i in wf_normed]
     # Calculate gradients and normalise them
     wf_grad = np.gradient(wf_for_grad)
     peak_gradient = wf_grad.max()
     wf_grad = wf_grad / float(peak_gradient)
     # Waveform finding logic
     start_indices = []
-    amplitude_threshold = 0.3
-    gradient_threshold = 0.3
+    amplitude_threshold = 0.2
+    gradient_threshold = 0.6
     running_lag = 0
-    running_lag_threshold = 10000
+    running_lag_threshold = 20000
+    backstep_offset = 1000
     for i in range(0,len(wf)):
-        if (wf[i] > amplitude_threshold and wf_grad[i] > gradient_threshold and running_lag >= running_lag_threshold):
-            start_indices.append(1)
+        if (wf_normed[i] > amplitude_threshold and wf_grad[i] > gradient_threshold and running_lag >= running_lag_threshold):
+            start_indices[i-backstep_offset] = 1
             running_lag = 0
         else:
             start_indices.append(0)
@@ -63,9 +64,24 @@ def upload_doppler(request):
     context['sample_length_secs'] = len(wf) / float(rate)
 
     num_points = 0
+    true_points = {}
     for i in range(0,len(start_indices)):
         if (start_indices[i] is not 0):
             num_points += 1
+            true_points[str(i)] = True
+    start_stop_list = true_points.keys()
+    wf_start_stop_points = []
+    start_stop_list = [int(i) for i in start_stop_list]
+    start_stop_list = sorted(start_stop_list)
+
+    for i in range(0,len(start_stop_list)):
+        if (i < len(start_stop_list)-1):
+            wf_start_stop_points.append((start_stop_list[i], start_stop_list[i+1]-1))
+    wf_list = []
+
+    for pair in wf_start_stop_points:
+        wf_list.append(wf[pair[0]:pair[1]])
+
     context['num_waveforms'] = num_points
 
     return JsonResponse(context)
